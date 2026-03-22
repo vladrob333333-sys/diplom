cdc0e3ffa78d1d019bdc2b67d11788bd2635381c
vladrob333333-sys <vladrob333333@gmail.com>
2026-03-22 15:03:40 +0300

    models.py

--- models.py
+++ models.py
@@ -16,6 +16,8 @@ class Employee(UserMixin, db.Model):
     full_name = db.Column(db.String(100))
     is_active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+        def get_id(self):
+        return f"emp_{self.id}"
 
     # Отношения
     created_tickets = db.relationship('Ticket', foreign_keys='Ticket.operator_id', backref='operator', lazy=True)
